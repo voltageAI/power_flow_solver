4faf95cd0cf017f7250da5de1a5bff7ed09b6cff
Mike Ratliff <mratliff@gmail.com>
2026-01-08 10:51:03 -0500

    Add correct checksums for v0.2.3

--- checksum-Elixir.PowerFlowSolver.Native.exs
+++ checksum-Elixir.PowerFlowSolver.Native.exs
@@ -1,4 +1,4 @@
 %{
-  "libpower_flow_solver-v0.2.3-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:920825a8c645bbc9539f214b1c384f970358ba6f55a9da6656445be3268d220e",
-  "libpower_flow_solver-v0.2.3-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:40454c9173bc89f057e163e46f227ea293d3450a73912cdbecba493490360c09",
+  "libpower_flow_solver-v0.2.3-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:78aaf7e2da0893bcdb768dad8693deddc2b88a230b00bf30a4e4416e195d1fa8",
+  "libpower_flow_solver-v0.2.3-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:32ceb7414c0ae5750707a3cd06b8a8622412b70fff7d687a3f36553b13630fdf",
 }
