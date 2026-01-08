dad20100f5c39b5867aacab7cb0942625932a5ff
Mike Ratliff <mratliff@gmail.com>
2026-01-08 10:43:39 -0500

    Bump to v0.2.3

--- checksum-Elixir.PowerFlowSolver.Native.exs
+++ checksum-Elixir.PowerFlowSolver.Native.exs
@@ -1,4 +1,4 @@
 %{
-  "libpower_flow_solver-v0.2.2-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:7ce9a7326f24b25bbc01141c1fe825a8b5e42273147dc7f82e38c5620c202545",
-  "libpower_flow_solver-v0.2.2-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:58ba4a7b5f60d26dfb189df37ef1397ba9c71359084abb33550f04144964dc5a",
+  "libpower_flow_solver-v0.2.3-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:920825a8c645bbc9539f214b1c384f970358ba6f55a9da6656445be3268d220e",
+  "libpower_flow_solver-v0.2.3-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:40454c9173bc89f057e163e46f227ea293d3450a73912cdbecba493490360c09",
 }
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PowerFlowSolver.MixProject do
   use Mix.Project
 
-  @version "0.2.2"
+  @version "0.2.3"
   @source_url "https://github.com/voltageAI/power_flow_solver"
 
   def project do
